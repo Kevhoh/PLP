e9ae72a4c9f3fb6847449305bb4987803b241793
Kelvin Githua <108420137+Kevhoh@users.noreply.github.com>
2024-02-27 00:51:21 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+#Ask the user to input their name and save it as name
+name = input('What is your Name?')
+#Ask the user to input their age and save it as"age"
+age = input('How old are you?')
+#ask user to input their location and save it as "location"
+location = input("Where do you live?")
+
+print(f"Hello {name}, You are {age} years old and lives in {location}.")
+#print(name)
